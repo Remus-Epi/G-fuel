f055cfb72cbe110ab73b865d46e0e5d27a083271
Remus-Epingeac <remus.epingeac@888holdings.com>
2025-06-17 23:25:31 +0300

    node project ready to deploy in Render 2

--- public/play.js
+++ public/play.js
@@ -1,12 +1,24 @@
 document.querySelector(".play-btn").addEventListener("click", function() {
-    const playButton = document.querySelector(".play-btn");
-    playButton.animate([
-        { transform: 'scale(1)' },
-        { transform: 'scale(1.4)' },
-        { transform: 'scale(1)' }
-    ], {
-        duration: 300,
-        easing: 'ease-in-out'
+    // const playButton = document.querySelector(".play-btn");
+    // playButton.animate([
+    //     { transform: 'scale(1)' },
+    //     { transform: 'scale(1.4)' },
+    //     { transform: 'scale(1)' }
+    // ], {
+    //     duration: 300,
+    //     easing: 'ease-in-out'
+    // });
+    // console.log("Play button clicked");
+    const pool = require('../src/utils/RenderDbConnection.js');
+
+    pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('DB error:', err);
+    } else {
+        console.log('Connected at:', res.rows[0]);
+    }
     });
-    console.log("Play button clicked");
+
 });
+
+
